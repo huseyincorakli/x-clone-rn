b802c75a70e938981cfc829869d9fd5675f26c78
Hüseyin Çoraklı <huseyincorakli46@gmail.com>
2025-08-10 03:02:15 +0300

    - notification routers & controllers added - arcjet configuration added

---
+++ backend/src/controllers/notification.controller.js
@@ -0,0 +1,40 @@
+import asyncHandler from "express-async-handler";
+import { getAuth } from "@clerk/express";
+import Notification from "../models/notification.model.js";
+import User from "../models/user.model.js";
+
+
+
+export const getNotifications = asyncHandler(async(req,res)=>{
+    const {userId} = getAuth(req);
+
+    const user = User.findOne({clerkId:userId})
+    if(!user) return res.status(404).json({error:"User not found"})
+
+    const notifications= await Notification.find({to:user._id})
+                        .sort({createAt:-1})
+                        .populate("from","username firstName lastName profilePicture")
+                        .populate("post","content image")
+                        .populate("comment","content")
+    
+    res.status(200).json({notifications})
+})
+
+
+export const deleteNotification = asyncHandler(async(req,res)=>{
+    const {userId}= getAuth(req);
+    const notificationId = req.params;
+
+    const user = User.findOne({clerkId:userId})
+    if(!user) return res.status(404).json({error:"User not found"})
+
+    const notification = Notification.findByIdAndDelete({
+        _id:notificationId,
+        to:user._id
+    })
+    
+    if(!notification) res.status(404).json({error:"Notification not found"})
+    
+    res.status(200).json({message:"Notification deleted"})
+})
+
--- backend/src/server.js
+++ backend/src/server.js
@@ -9,6 +9,8 @@ import { connectDB } from "./config/db.js";
 import userRoutes from './routers/user.route.js'
 import postsRoutes from './routers/post.route.js'
 import commentRoutes from './routers/comment.route.js'
+import notificationRoutes from './routers/notification.route.js'
+import { arcjetMiddleWare } from "./middleware/arcjet.middleware.js";
 
 const app = express();
 
@@ -16,11 +18,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
-
+app.use(arcjetMiddleWare)
 
 app.use("api/users",userRoutes)
 app.use("api/posts",postsRoutes)
 app.use("api/comment",commentRoutes)
+app.use("api/notification",notificationRoutes)
 
 app.use((err,req,res,next)=>{
   console.error("Unhandled error",err);
