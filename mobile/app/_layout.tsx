f9b1ad12756bfdde6f865b3259fd07f3ac3de2c1
Hüseyin Çoraklı <huseyincorakli46@gmail.com>
2025-08-11 02:48:38 +0300

    search page added

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -25,8 +25,8 @@ function InnerLayout() {
 
   return (
     <Stack screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="(auth)" options={{ headerShown: false }} />
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+      <Stack.Screen name="(auth)"/>
+      <Stack.Screen name="(tabs)"/>
     </Stack>
   );
 }
