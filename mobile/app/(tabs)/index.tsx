f539b370d8ee80afd8e4644ac3eca3c9de012423
Hüseyin Çoraklı <huseyincorakli46@gmail.com>
2025-08-13 12:06:35 +0300

    fix field userName

--- mobile/app/(tabs)/index.tsx
+++ mobile/app/(tabs)/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Button } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
 import SignOutButton from '@/components/SignOutButton'
@@ -6,27 +6,9 @@ import { useUserSync } from '@/hooks/useUserSync'
 
 const HomeScreen = () => {
   useUserSync();
-
- const calistir = async () => {
-  try {
-    const r = await fetch('https://xwsg4swwscgo0cs00wgkgosg.corhus.space/health');
-
-    if (!r.ok) {
-      console.log(`HTTP Hatası: ${r.status}`);
-      return;
-    }
-
-    const data = await r.json(); // Yanıt JSON formatındaysa
-    console.log(data);           // JSON'u konsola yazdır
-  } catch (error) {
-    console.error('Fetch hatası:', error);
-  }
-};
-
   return (
     <SafeAreaView className='flex-1'>
       <Text>index2</Text>
-      <Button onPress={()=>{calistir()}} title='Tıkla'></Button>
       <SignOutButton/>
     </SafeAreaView>
   )
